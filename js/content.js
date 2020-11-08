a12097cc3280bdba75642536323c6bf6ddac2afa
KimDonghyeok <qoophon@gmail.com>
2020-11-08 15:26:43 +0900

    update

--- js/content.js
+++ js/content.js
@@ -1,3 +1,4 @@
+/*
 // 네이버 검색 결과에서 블로그 섹션을 찾아낸 뒤 블로그 검색결과 리스트의 각 요소 위에 분석 내용을 삽입하기 위한 html 요소 생성
 let blog_section = document.querySelector("._blogBase")
 let blog_list = blog_section.querySelector(".type01")
@@ -14,3 +15,8 @@ analyze_info.appendChild(analyze_content)
 info_container.appendChild(analyze_info)
 
 blog_list.insertBefore(analyze_info, blog_list.childNodes[0])
+*/
+
+/**
+ *
+ */
