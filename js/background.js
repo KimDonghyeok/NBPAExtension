e766f79dbe8853ea50ef7699523f9991e9ed61bd
KimDonghyeok <qoophon@gmail.com>
2020-11-12 11:37:10 +0900

    update

--- js/background.js
+++ js/background.js
@@ -1,48 +1,12 @@
+console.log("execute background script")
+
 /*
-
-/!**
- * 팝업에서 search_naver_popup 요소를 보여주고 blog_naver_popup 요소를 숨김
-let showSearchNaverPopup = () => {
-    let tagSearchNaver = document.getElementsByClassName('search_naver_popup').item(0)
-    let tagBlogNaver = document.getElementsByClassName('blog_naver_popup').item(0)
-
-    tagSearchNaver.style.display = 'block'
-    tagBlogNaver.style.display = 'none'
-}
-
-/!**
- * 팝업에서 blog_naver_popup  요소를 보여주고 search_naver_popup 요소를 숨김
- *!/
-let showBlogNaverPopup = () => {
-    let tagSearchNaver = document.querySelector('.search_naver_popup')
-    let tagBlogNaver = document.querySelector('.blog_naver_popup')
-
-    tagSearchNaver.setAttribute('style', 'display: none')
-    tagBlogNaver.setAttribute('style', 'display: block')
-}*!/
+chrome.tabs.onActivated.addListener(tab =>{
+    chrome.tabs.get(tab.tabId, tabInfo => {
+        if (/^https:\/\/www.google/.test(tabInfo.url)) {
+            chrome.tabs.executeScript(null, {file: './js/content.js'}, () => console.log('script injected'))
+        }
+    })
+})
 */
 
-// 매치되는 URL 에 따라 showSearchNaverPopup 함수를 실행하는 rule 정의
-let showSearchNaverPopupRule = {
-    conditions: [new chrome.declarativeContent.PageStateMatcher({
-        pageUrl: {hostEquals: 'search.naver.com'}
-    })
-    ],
-    actions: [new chrome.declarativeContent.ShowPageAction()]
-};
-
-// 매치되는 URL 에 따라 showBlogNaverPopup 함수를 실행하는 rule 정의
-let showBlogNaverPopupRule = {
-    conditions: [new chrome.declarativeContent.PageStateMatcher({
-        pageUrl: {hostEquals: 'blog.naver.com'}
-    })],
-    actions: [new chrome.declarativeContent.ShowPageAction()]
-}
-
-chrome.runtime.onInstalled.addListener(function () {
-
-    chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
-        chrome.declarativeContent.onPageChanged.addRules([showSearchNaverPopupRule]);
-        chrome.declarativeContent.onPageChanged.addRules([showBlogNaverPopupRule]);
-    });
-});
--- js/content.js
+++ js/content.js
@@ -1,3 +1,5 @@
+console.log("execute content script")
+
 /*
 // 네이버 검색 결과에서 블로그 섹션을 찾아낸 뒤 블로그 검색결과 리스트의 각 요소 위에 분석 내용을 삽입하기 위한 html 요소 생성
 let blog_section = document.querySelector("._blogBase")
@@ -16,3 +18,4 @@ info_container.appendChild(analyze_info)
 
 blog_list.insertBefore(analyze_info, blog_list.childNodes[0])
 */
+
