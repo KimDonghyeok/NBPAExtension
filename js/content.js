ef58988bbf9d33c8c491b73a15d78fa8ee1c88c1
KimDonghyeok <qoophon@gmail.com>
2020-11-12 17:17:35 +0900

    Update content.js

--- js/content.js
+++ js/content.js
@@ -1,21 +1,40 @@
 console.log("execute content script")
 
-/*
-// 네이버 검색 결과에서 블로그 섹션을 찾아낸 뒤 블로그 검색결과 리스트의 각 요소 위에 분석 내용을 삽입하기 위한 html 요소 생성
-let blog_section = document.querySelector("._blogBase")
-let blog_list = blog_section.querySelector(".type01")
 
-let info_container = document.createElement("li")
-let analyze_info = document.createElement("div")
+// 통합검색결과에서 View 섹션의 리스트를 가져옴
+let viewSection = document.getElementsByClassName('_au_view_collection').item(0)
+let viewList = viewSection.getElementsByClassName('_list_base').item(0)
 
-info_container.setAttribute("class", "info_container")
-analyze_info.setAttribute("class", "analyze_info")
+console.log(viewList)
+console.log(typeof (viewList))
+console.log(viewList.childElementCount)
 
-let analyze_content = document.createTextNode("분석정보입니다.")
+//viewList 의 노드를 받아서 내부의 a 태그의 href 값을 통해 블로그 URL 인지 판별
+let isBlogSection = (element) => {
+    const REG_EXP = new RegExp(/^https:\/\/blog\.naver\.com/)
 
-analyze_info.appendChild(analyze_content)
-info_container.appendChild(analyze_info)
+    let currentNodeUrl = element.childNodes[1].childNodes[1].href
 
-blog_list.insertBefore(analyze_info, blog_list.childNodes[0])
-*/
+    return REG_EXP.test(currentNodeUrl);
+}
 
+// 통합검색결과에서 가져온 viewList 하위의 블로그 게시물 상단에 분석정보 컨테이너를 생성
+let createAnalyzeInfoContainer = (list) => {
+
+    for (let i = 1; i <= list.childElementCount; i++) {
+
+        if(isBlogSection(list.childNodes[i])) {
+            let info_container = document.createElement("li")
+            let analyze_info = document.createElement("div")
+
+            let analyze_content = document.createTextNode("분석정보입니다.")
+
+            analyze_info.appendChild(analyze_content)
+            info_container.appendChild(analyze_info)
+
+            list.insertBefore(info_container, list.childNodes[i])
+        }
+    }
+}
+
+// createAnalyzeInfoContainer(viewList)
