aa0f47fc5fdc401f1ddf6a23c1f15b1cd24fa179
dldhk97 <dldhk97@gmail.com>
2020-11-22 20:00:28 +0900

    팝업 코드 정리중

--- js/search_naver_popup_script.js
+++ js/search_naver_popup_script.js
@@ -11,7 +11,7 @@ class SearchPopup {
 window.onload = function() {
     console.log("onload" + Date())
 
-    // 체크박스 리스너 등록
+    // 보기설정 체크박스 리스너 등록
     checkbox_lorem_poss_view = document.getElementById("lorem-poss-view")
     addCheckboxListener(checkbox_lorem_poss_view)
 
@@ -24,12 +24,31 @@ window.onload = function() {
     checkbox_imoticon_ratio_view = document.getElementById("imoticon-ratio-view")
     addCheckboxListener(checkbox_imoticon_ratio_view)
 
+    // 필터설정 체크박스 리스너 등록
+    checkbox_lorem_poss_filter = document.getElementById("lorem-poss-filter")
+    addCheckboxListener(checkbox_lorem_poss_filter)
+
+    checkbox_image_ratio_filter = document.getElementById("image-ratio-filter")
+    addCheckboxListener(checkbox_image_ratio_filter)
+
+    checkbox_video_ratio_filter = document.getElementById("video-ratio-filter")
+    addCheckboxListener(checkbox_video_ratio_filter)
+
+    checkbox_imoticon_ratio_filter = document.getElementById("imoticon-ratio-filter")
+    addCheckboxListener(checkbox_imoticon_ratio_filter)
+
     // 스토리지와 체크박스를 동기화
     syncCheckboxStatus(checkbox_lorem_poss_view)
     syncCheckboxStatus(checkbox_image_ratio_view)
     syncCheckboxStatus(checkbox_video_ratio_view)
     syncCheckboxStatus(checkbox_imoticon_ratio_view)
+    syncCheckboxStatus(checkbox_lorem_poss_filter)
+    syncCheckboxStatus(checkbox_image_ratio_filter)
+    syncCheckboxStatus(checkbox_video_ratio_filter)
+    syncCheckboxStatus(checkbox_imoticon_ratio_filter)
 }
+
+/* ------------------------- 팝업 요소 상태 처리 함수 정의 ------------------------- */
 
 
 // 불리언을 checked/unchecked로 변환, undefined면 defaultValue 반환
@@ -61,72 +80,133 @@ let addCheckboxListener = (checkbox) =>{
 
 // checkbox의 id로 실행할 메소드를 실행
 let checkboxMethodSelector = (checkbox) => {
+    let checkboxId = checkbox.id
     let boolean = checkbox.checked
-    if(checkbox.id === "lorem-poss-view"){
-        // 로렘 확률 ON/OFF(boolean) 함수 호출바람.
-    } else if(checkbox.id === "image-ratio-view"){
-        // 이미지 비율 ON/OFF(boolean) 함수 호출바람.
-    } else if(checkbox.id === "video-ratio-view"){
-        // 영상 비율 ON/OFF(boolean) 함수 호출바람.
-    } else if(checkbox.id === "imoticon-ratio-view"){
-        // 이모티콘 비율 ON/OFF(boolean) 함수 호출바람.
+
+    switch(checkboxId){
+        case "lorem-poss-view":
+            // 로렘 확률 표시 함수 호출
+            break
+        case "image-ratio-view":
+            // 이미지 비율 표시 함수 호출
+            break
+        case "video-ratio-view":
+            // 영상 비율 표시 함수 호출
+            break
+        case "imoticon-ratio-view":
+            // 이모티콘 비율 표시 함수 호출
+            break
+        case "lorem-poss-filter":
+        case "image-ratio-filter":
+        case "video-ratio-filter":
+        case "imoticon-ratio-filter":
+            // 필터 활성/비활성화 함수 호출
+            sliderId = getSliderId(checkboxId)
+            changeSliderStatus(sliderId, boolean)
+            break
     }
     
 }
 
-// ----------------- 스토리지 관련 하드코딩 ---------------------- //
+/* ------------------------- 로직 ------------------------- */
+
+// 체크박스 ID를 주면 슬라이더 ID를 반환
+let getSliderId = (checkboxId) =>{
+    switch (checkboxId){
+        case "lorem-poss-filter":
+            return "lorem-poss-slider"
+        case "image-ratio-filter":
+            return "image-ratio-slider"
+        case "video-ratio-filter":
+            return "video-ratio-slider"
+        case "imoticon-ratio-filter":
+            return "imoticon-ratio-slider" 
+    }
+}
+
+// 슬라이더 ID와 활성화여부를 받으면 슬라이더를 활성화/비활성화 함
+let changeSliderStatus = (sliderId, enabled) =>{
+    slider = document.getElementById(sliderId)
+    slider.disabled = !enabled
+}
+
+/* ------------------------- 스토리지 관련 하드코딩 ------------------------- */
 
 // 체크박스 스토리지 저장
 let saveCheckboxStatus = (checkbox) =>{
-
     let checked_string = booleanToCheck(checkbox.checked, false)
 
-    if(checkbox.id === "lorem-poss-view"){
-        chrome.storage.local.set({"lorem_poss_view": checked_string},function(){
-            console.log("lorem_poss_view " + checked_string + " saved")
-        })
-    } else if(checkbox.id === "image-ratio-view"){
-        chrome.storage.local.set({"image_ratio_view": checked_string},function(){
-            console.log("image_ratio_view " + checked_string + " saved")
-        })
-    }
-    else if(checkbox.id === "video-ratio-view"){
-        chrome.storage.local.set({"video_ratio_view": checked_string},function(){
-            console.log("video_ratio_view " + checked_string + " saved")
-        })
-    }
-    else if(checkbox.id === "imoticon-ratio-view"){
-        chrome.storage.local.set({"imoticon_ratio_view": checked_string},function(){
-            console.log("imoticon_ratio_view " + checked_string + " saved")
-        })
+    switch(checkbox.id){
+        case "lorem-poss-view":
+            chrome.storage.local.set({"lorem_poss_view": checked_string},function(){})
+            break
+        case "image-ratio-view":
+            chrome.storage.local.set({"image_ratio_view": checked_string},function(){})
+            break
+        case "video-ratio-view":
+            chrome.storage.local.set({"video_ratio_view": checked_string},function(){})
+            break
+        case "imoticon-ratio-view":
+            chrome.storage.local.set({"imoticon_ratio_view": checked_string},function(){})
+            break
+        case "lorem-poss-filter":
+            chrome.storage.local.set({"lorem_poss_filter": checked_string},function(){})
+            break
+        case "image-ratio-filter":
+            chrome.storage.local.set({"image_ratio_filter": checked_string},function(){})
+            break
+        case "video-ratio-filter":
+            chrome.storage.local.set({"video_ratio_filter": checked_string},function(){})
+            break
+        case "imoticon-ratio-filter":
+            chrome.storage.local.set({"imoticon_ratio_filter": checked_string},function(){})
+            break
     }
 }
 
 // 체크박스를 주면 스토리지에서 동기화
 let syncCheckboxStatus = (checkbox) =>{
-    if(checkbox.id === "lorem-poss-view"){
-        chrome.storage.local.get("lorem_poss_view",function(obj){
-            bool = checkToBoolean(obj.lorem_poss_view, true)
-            checkbox.checked = bool
-        })
-    } 
-    else if(checkbox.id === "image-ratio-view"){
-        chrome.storage.local.get("image_ratio_view",function(obj){
-            bool = checkToBoolean(obj.image_ratio_view, true)
-            checkbox.checked = bool
-        })
-    }
-    else if(checkbox.id === "video-ratio-view"){
-        chrome.storage.local.get("video_ratio_view",function(obj){
-            bool = checkToBoolean(obj.video_ratio_view, true)
-            checkbox.checked = bool
-        })
-    }
-    else if(checkbox.id === "imoticon-ratio-view"){
-        chrome.storage.local.get("imoticon_ratio_view",function(obj){
-            bool = checkToBoolean(obj.imoticon_ratio_view, true)
-            checkbox.checked = bool
-        })
+    switch(checkbox.id){
+        case "lorem-poss-view":
+            chrome.storage.local.get("lorem_poss_view",function(obj){
+                checkbox.checked = checkToBoolean(obj.lorem_poss_view, true)
+            })
+            break
+        case "image-ratio-view":
+            chrome.storage.local.get("image_ratio_view",function(obj){
+                checkbox.checked = checkToBoolean(obj.image_ratio_view, true)
+            })
+            break
+        case "video-ratio-view":
+            chrome.storage.local.get("video_ratio_view",function(obj){
+                checkbox.checked = checkToBoolean(obj.video_ratio_view, true)
+            })
+            break
+        case "imoticon-ratio-view":
+            chrome.storage.local.get("imoticon_ratio_view",function(obj){
+                checkbox.checked = checkToBoolean(obj.imoticon_ratio_view, true)
+            })
+            break
+        case "lorem-poss-filter":
+            chrome.storage.local.get("lorem_poss_filter",function(obj){
+                checkbox.checked = checkToBoolean(obj.lorem_poss_filter, false)
+            })
+            break
+        case "image-ratio-filter":
+            chrome.storage.local.get("image_ratio_filter",function(obj){
+                checkbox.checked = checkToBoolean(obj.image_ratio_filter, false)
+            })
+            break
+        case "video-ratio-filter":
+            chrome.storage.local.get("video_ratio_filter",function(obj){
+                checkbox.checked = checkToBoolean(obj.video_ratio_filter, false)
+            })
+            break
+        case "imoticon-ratio-filter":
+            chrome.storage.local.get("imoticon_ratio_filter",function(obj){
+                checkbox.checked = checkToBoolean(obj.imoticon_ratio_filter, false)
+            })
+            break
     }
     
 }
